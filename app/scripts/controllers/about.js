f5b0fcebf5e25acb04e821379db516bd9470e262
Amit Dubey <me@amitdubey.in>
2015-07-07 01:15:06 +0530

    first commit, environment setup, yeoman scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gitRepoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gitRepoApp
+ */
+angular.module('gitRepoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gitRepoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gitRepoApp
+ */
+angular.module('gitRepoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+
+  });
